c26ac9c3f64f44139cfa26a9a01429351b94335b
HenrikGabrelsson <hg222aa@student.lnu.se>
2014-11-05 11:22:30 +0000

    tid mellan skrapningar fixat

--- node.js
+++ node.js
@@ -1,17 +1,42 @@
-var ns = require('node-static'); //för att serva filer till klienten
-var app = require('http').createServer(handler); 
-
-app.listen(8080); //lyssna genom denna port.
-
-
-//Game/Client är mappen där alla publika filer ligger
-var fileServer = new ns.Server('./1DV449_hg222aa/client', {cache: 10}); 
+var globals =
+{
+    app: require("http").createServer(handler), 
+    lastScrape: null, //senaste tidpunkten som en skrapning genomfördes
+    timeBetweenScrapes: 300000 //tid mellan varje skrapning (5 minuter i millisekunder)
+};
 
 
 //När en klient ansluter körs denna funktion.
-function handler (req, res) {
-    req.addListener('end', function () {
-        fileServer.serve(req, res); //skicka filer
-
-    }).resume();
+function handler (req, res) 
+{
+    Scrape();
+    
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write
+    (
+        "<!doctype html>"+
+        "<html>"+
+        "<head>"+
+        "</head>"+
+        "<body>"+
+        "<p>"+globals.lastScrape+"</p>"+
+        "</body>"+
+        "</html>"
+    );
+    res.end();
 }
+
+
+//den här funktionen ska "skrapa" ner resultat från en extern html-sida
+function Scrape()
+{
+    //om det har gått en viss tid sedan senaste skrapningen så ska den göras igen.
+    if(globals.lastScrape === null || Date.now()-globals.lastScrape >= globals.timeBetweenScrapes)
+    {
+        console.log("scrape");
+        globals.lastScrape = Date.now();
+    }
+    
+}
+
+globals.app.listen(8080); //lyssna genom denna port.
