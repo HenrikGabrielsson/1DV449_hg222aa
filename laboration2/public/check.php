11f1f8f906646a5e9f041edfb6fcaa9f5a35c13a
HenrikGabrielsson <hg222aa@student.lnu.se>
2014-11-25 15:55:35 +0100

    lagt till lite mer säkerhet

--- laboration2/public/check.php
+++ laboration2/public/check.php
@@ -8,17 +8,16 @@ $p = $_POST['password'];
 
 // Check if user is OK
 if(isset($u) && isset($p) && isUser($u, $p)) {
-	// set the session
-	sec_session_start();
-	$_SESSION['username'] = $u;
+    // set the session
+    sec_session_start();
+    $_SESSION['username'] = $u;
     $_SESSION['ip'] = $_SERVER["REMOTE_ADDR"];
     $_SESSION['useragent'] = $_SERVER['HTTP_USER_AGENT'];
-	
-	header("Location: mess.php"); 
+
+    header("Location: mess.php");
 }
 else {
-	// To bad
-	header('HTTP/1.1 401 Unauthorized');
-	die("could not call");
+    // To bad
+    header('HTTP/1.1 401 Unauthorized');
+    die("could not call");
 }
-
--- laboration2/public/post.php
+++ laboration2/public/post.php
@@ -1,57 +1,42 @@
 <?php
 
 /**
-* Called from AJAX to add stuff to DB
-*/
+ * Called from AJAX to add stuff to DB
+ */
 function addToDB($message, $user) {
-	$db = null;
+    $db = null;
 
 
     //se till så att html-taggar i input inte tolkas av en webbläsare
     $message = htmlspecialchars($message);
     $user = htmlspecialchars($user);
-	
-	try {
-		$db = new PDO("sqlite:../db.db");
-		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	}
-	catch(PDOEcception $e) {
-		die("Something went wrong -> " .$e->getMessage());
-	}
 
-	$q = "SELECT * FROM users WHERE username = ?";
-	$params = array($user);
-	$result;
-	$stm;
-	try {
-		$stm = $db->prepare($q);
-		$stm->execute($params);
-		$result = $stm->fetchAll();
-		if(!$result) {
-			return "Could not find the user";
-		}
-	}
-	catch(PDOException $e) {
-		echo("Error creating query: " .$e->getMessage());
-		return false;
-	}
+    //koll så att har skrivit sitt riktiga namn. Vet inte varför, men så är det.
+    if(!isset($_SESSION["username"]) || $_SESSION["username"] != $user )
+    {
+        return;
+    }
 
-	
-	$q = "INSERT INTO messages (message, name, date) VALUES(?,?,?)";
-	$params = array($message, $user, time());
-	try {
-		$query = $db->prepare($q);
-		$result = $query->execute($params);
+    try {
+        $db = new PDO("sqlite:../db.db");
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOEcception $e) {
+        die("Something went wrong -> " .$e->getMessage());
+    }
 
-	}
-	catch(PDOException $e) 
-	{
-		return;
-	}
-	
+    $result;
 
-	// Send the message back to the client
-	echo "Message saved by user: " .json_encode($result);
-	
+    $q = "INSERT INTO messages (message, name, date) VALUES(?,?,?)";
+    $params = array($message, $user, time());
+    try {
+        $query = $db->prepare($q);
+        $result = $query->execute($params);
+
+    }
+    catch(PDOException $e)
+    {
+        return;
+    }
+
 }
-
