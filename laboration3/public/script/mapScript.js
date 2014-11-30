205868403006e62ff48f0d59bc26fe83b4c6cbf2
HenrikGabrelsson <hg222aa@student.lnu.se>
2014-11-30 23:06:57 +0000

    lagt till lite i mapScript

--- laboration3/public/script/mapScript.js
+++ laboration3/public/script/mapScript.js
@@ -11,9 +11,52 @@ var loadMap = function()
 
 socket.on("trafficMessages", function(json)
 {
-    alert(json.dataRecievedTime);
+    updateAboutSection(json.dataRecievedTime, json.copyright)
 })
 
+var updateAboutSection = function(date, copyright)
+{
+    
+    var lastUpdateDiv = document.getElementsByClassName("lastUpdate")[0];
+    var copyrightDiv = document.getElementsByClassName("copyright")[0];
+    
+    var lastUpdateText = document.createTextNode("Senaste uppdatering: " + getDateString(date));
+    var copyrightText = document.createTextNode(copyright);
+    
+    lastUpdateDiv.appendChild(lastUpdateText);
+    copyrightDiv.appendChild(copyrightText);
+}
+
+var getDateString = function(timestamp)
+{
+    
+    var addZero = function(time)
+    {
+        if(time < 10)
+        {
+            return "0" + time; 
+        }
+        return time;
+    }
+    
+    var dateObject = new Date(timestamp);
+    
+    var months = ["Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli", "Augusti", "September", "Oktober", "November", "December"];
+    
+    var year = dateObject.getFullYear();
+    var month = months[dateObject.getMonth()];
+    var date = dateObject.getDate();
+    var hour = dateObject.getHours();
+    var minute = dateObject.getMinutes();
+    var second = dateObject.getSeconds();
+    
+    hour = addZero(hour);
+    minute = addZero(minute);
+    second = addZero(second);
+    
+    return hour + ":" + minute + ":" + second + " " + date + " " + month + " " + year;
+    
+}
 
 //ladda in kartan när sidan laddat klart.
 google.maps.event.addDomListener(window, 'load', loadMap);
